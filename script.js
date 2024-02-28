b0e7f540f01709b59ed3904f00eb99119d27c9dc
Diana Makarova <din_m-v@mail.ru>
2024-02-28 13:47:31 +0300

    работа с многомерными массивами, условием if, конструкцией switch, и тернарным оператором

--- script.js
+++ script.js
@@ -16,3 +16,43 @@ console.log(raisedInDegree);
 // 4. Первые 2 цифры полученного числа
 let firstTwoDigits = +String(raisedInDegree).slice(0,2);
 console.log(firstTwoDigits);
+
+// lesson03
+
+let lang = prompt("Введите 'ru' или 'en'");
+// через if
+if(lang == "ru") {
+	console.log("Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье");
+}else if (lang == "en") {
+	console.log("Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday");
+}else {
+	console.log("Ввели не верное значение");
+}
+
+// через switch-case
+switch (lang) {
+	case 'ru':
+		console.log("Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье");
+		break;
+	case 'en':
+		console.log("Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday");
+		break;
+	default:
+		console.log("Ввели не верное значение");
+		break;
+}
+
+// через многомерный массив 
+const weekday = [] ;
+weekday['ru'] = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
+weekday['en'] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+console.log(weekday[lang]);
+
+let namePerson = prompt("Введите имя");
+// с использованием тернарного оператора
+let user = (namePerson == "Артем") ? console.log("директор") :
+		(namePerson == "Александр") ? console.log("преподаватель") :
+		console.log("студент");
+
+
+
